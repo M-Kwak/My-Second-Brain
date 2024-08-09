36b3865eb44d432fa91861066264cbedef49d595
M-Kwak <prospirev@gmail.com>
2024-08-09 21:16:50 +0200

    Manage large mobile (#5)
    * Set container comp width to 100%
    
    * Add flex to body and root container
    
    * Add a container to LoginActionPannel inner content to not be limited by mainContainer width and height
    
    * Add weight and height to styled inputs
    
    * Rework loginActionPannel main container and login form container.
    Add breakpoints.

--- src/pages/login/LoginactionPannel/LoginActionPannel.tsx
+++ src/pages/login/LoginactionPannel/LoginActionPannel.tsx
@@ -9,27 +9,29 @@ function LoginActionPannel(): React.JSX.Element {
       direction="column"
       id="mainContainer"
     >
-      <h1>Login</h1>
-      <form id="loginFormContainer">
-        <StyledInput
-          type="text"
-          placeholder="Email"
-          name="login"
-        />
-        <StyledInput
-          type="text"
-          placeholder="Password"
-          name="login"
-        />
-        <span id="forgotPassword">Forgot Password ?</span>
-        <StyledSubmitInput
-          value="Login"
-          name="login"
-        />
-      </form>
-      <Container direction="row">
-        <p>Don't have an account yet ?</p>
-        <span id="createAccount">Create</span>
+      <Container direction="column">
+        <h1>Login</h1>
+        <form id="loginFormContainer">
+          <StyledInput
+            type="text"
+            placeholder="Email"
+            name="login"
+          />
+          <StyledInput
+            type="text"
+            placeholder="Password"
+            name="login"
+          />
+          <span id="forgotPassword">Forgot Password ?</span>
+          <StyledSubmitInput
+            value="Login"
+            name="login"
+          />
+        </form>
+        <Container direction="row">
+          <p>Don't have an account yet ?</p>
+          <span id="createAccount">Create</span>
+        </Container>
       </Container>
     </Container>
   );
