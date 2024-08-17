7b4c08042f6215153933eb712edb6882ac5ba84e
M-Kwak <prospirev@gmail.com>
2024-08-18 00:42:45 +0200

    Add range of values to loginPageContent types in types.ts to reflect every login page content possible.

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,2 +1,11 @@
-export type content = "login" | "create" | "reset";
+export type loginPageContent =
+    "login" |
+    "create-account-info" |
+    "create-account-email-success" |
+    "create-account-success" |
+    "reset-pwd-info" |
+    "reset-pwd-email-success" |
+    "reset-pwd" |
+    "reset-pwd-success";
+
 export type direction = 'row' | 'column';
