f082bf9577a3aee490558d69858df286706d4cdf
M-Kwak <prospirev@gmail.com>
2024-08-18 00:43:59 +0200

    Add every login page content associated component to LoginActionPannel according to its custom type.

--- src/pages/login/LoginactionPannel/LoginActionPannel.tsx
+++ src/pages/login/LoginactionPannel/LoginActionPannel.tsx
@@ -1,13 +1,17 @@
 import "./LoginActionPannel.css";
 import { useState } from "react";
-import { content } from "../../../types/types";
+import { loginPageContent } from "../../../types/types";
 import Container from "../../../components/container/Container";
 import LoginContent from "./loginContent/LoginContent";
-import CreateAccountContent from "./createAccountContent/CreateAccountContent";
-import ResetPasswordContent from "./resetPasswordContent/ResetPasswordContent";
+import AccountEmailSent from "./accountCreation/accountEmailSent/AccountEmailSent";
+import AccountCreationSuccess from "./accountCreation/accountCreationSuccess/AccountCreationSuccess";
+import ResetPwdUserEmail from "./resetPassword/resetPwdUserEmail/ResetPwdUserEmail";
+import ResetPwdSuccess from "./resetPassword/resetPwdSuccess/ResetPwdSuccess";
+import ResetPwd from "./resetPassword/resetPwd/ResetPwd";
+import AccountUserInfo from "./accountCreation/accountUserInfo/AccountUserInfo";
 
 function LoginActionPannel(): React.JSX.Element {
-  const [pannelPage, setPannelPage] = useState<content>("login");
+  const [pannelPage, setPannelPage] = useState<loginPageContent>("login");
 
   return (
     <Container
@@ -16,8 +20,13 @@ function LoginActionPannel(): React.JSX.Element {
     >
       <Container direction="column">
         {pannelPage === "login" && <LoginContent setPannelPage={setPannelPage} />}
-        {pannelPage === "create" && <CreateAccountContent setPannelPage={setPannelPage} />}
-        {pannelPage === "reset" && <ResetPasswordContent setPannelPage={setPannelPage} />}
+        {pannelPage === "create-account-info" && <AccountUserInfo setPannelPage={setPannelPage} />}
+        {pannelPage === "create-account-email-success" && <AccountEmailSent setPannelPage={setPannelPage} />}
+        {pannelPage === "create-account-success" && <AccountCreationSuccess setPannelPage={setPannelPage} />}
+        {pannelPage === "reset-pwd-info" && <ResetPwdUserEmail setPannelPage={setPannelPage} />}
+        {pannelPage === "reset-pwd-email-success" && <ResetPwdSuccess setPannelPage={setPannelPage} />}
+        {pannelPage === "reset-pwd" && <ResetPwd setPannelPage={setPannelPage} />}
+        {pannelPage === "reset-pwd-success" && <ResetPwdSuccess setPannelPage={setPannelPage} />}
       </Container>
     </Container>
   );
