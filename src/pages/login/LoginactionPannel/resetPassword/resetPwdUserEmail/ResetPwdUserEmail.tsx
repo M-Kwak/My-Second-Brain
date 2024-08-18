04070de94b2cf70ea4bea7ad5da24b52b376cf92
M-Kwak <prospirev@gmail.com>
2024-08-18 17:56:25 +0200

    Reset pwd info content (#15)
    * Fix css in AccountCreationSuccess and AccountCreationEmailSent by calling their forgotten root container.
    
    * Add reset password content + css + responsive
    
    * fix indent in ResetPwdUSerEmail

--- src/pages/login/LoginactionPannel/resetPassword/resetPwdUserEmail/ResetPwdUserEmail.tsx
+++ src/pages/login/LoginactionPannel/resetPassword/resetPwdUserEmail/ResetPwdUserEmail.tsx
@@ -1,6 +1,9 @@
 import { Dispatch, SetStateAction } from "react";
 import Container from "../../../../../components/container/Container";
 import { loginPageContent } from "../../../../../types/types";
+import StyledInput from "../../../../../components/styledInput/StyledInput";
+import StyledSubmitInput from "../../../../../components/styledSubmitInput.tsx/StyledSubmitInput";
+import "./ResetPwdUserEmail.css";
 
 interface ResetPwdUserEmailSpecs {
   setPannelPage: Dispatch<SetStateAction<loginPageContent>>,
@@ -9,9 +12,30 @@ interface ResetPwdUserEmailSpecs {
 function ResetPwdUserEmail(props: ResetPwdUserEmailSpecs): React.JSX.Element {
   const { setPannelPage } = props;
   return (
-    <Container direction="column">
-      <p>RESET PWD USER EMAIL</p>
-      <p onClick={() => setPannelPage("login")}>GO BACK</p>
+    <Container
+      direction="column"
+      id="ResetPwdUserEmail"
+    >
+      <h1>Reset password</h1>
+      <Container
+        direction="column"
+        id="content"
+      >
+        <p>Enter your email address and get a link to reset your password</p>
+        <form>
+          <StyledInput
+            name="resetPwdInfo"
+            placeholder="Email"
+            id="EmailInput"
+          />
+          <StyledSubmitInput
+            name="resetPwdInfo"
+            value="Send an email"
+            onClick={() => setPannelPage("reset-pwd-email-success")}
+          />
+        </form>
+      </Container>
+      <span onClick={() => setPannelPage("login")}>return</span>
     </Container>
   );
 }
