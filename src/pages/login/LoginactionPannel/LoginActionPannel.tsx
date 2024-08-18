47145fd49d3bbd8af6ed067e5a8af3c8ec4049ce
M-Kwak <prospirev@gmail.com>
2024-08-18 15:00:45 +0200

    Add content for account creation email sent component. - add svg email icon - add onClick property to styledSubmitInput - add css + responsive - rename AccountEmailSent to AccountCreationEmailSent Also fix component order in LoginActionPannel for better readability

--- src/pages/login/LoginactionPannel/LoginActionPannel.tsx
+++ src/pages/login/LoginactionPannel/LoginActionPannel.tsx
@@ -3,12 +3,13 @@ import { useState } from "react";
 import { loginPageContent } from "../../../types/types";
 import Container from "../../../components/container/Container";
 import LoginContent from "./loginContent/LoginContent";
-import AccountEmailSent from "./accountCreation/accountEmailSent/AccountEmailSent";
+import AccountCreationUserInfo from "./accountCreation/accountCreationUserInfo/AccountCreationUserInfo";
+import AccountCreationEmailSent from "./accountCreation/accountEmailSent/AccountCreationEmailSent";
 import AccountCreationSuccess from "./accountCreation/accountCreationSuccess/AccountCreationSuccess";
 import ResetPwdUserEmail from "./resetPassword/resetPwdUserEmail/ResetPwdUserEmail";
-import ResetPwdSuccess from "./resetPassword/resetPwdSuccess/ResetPwdSuccess";
+import ResetPwdEmailSent from "./resetPassword/resetPwdEmailSent/ResetPwdEmailSent";
 import ResetPwd from "./resetPassword/resetPwd/ResetPwd";
-import AccountCreationUserInfo from "./accountCreation/accountCreationUserInfo/AccountCreationUserInfo";
+import ResetPwdSuccess from "./resetPassword/resetPwdSuccess/ResetPwdSuccess";
 
 function LoginActionPannel(): React.JSX.Element {
   const [pannelPage, setPannelPage] = useState<loginPageContent>("login");
@@ -21,10 +22,10 @@ function LoginActionPannel(): React.JSX.Element {
       <Container direction="column">
         {pannelPage === "login" && <LoginContent setPannelPage={setPannelPage} />}
         {pannelPage === "create-account-info" && <AccountCreationUserInfo setPannelPage={setPannelPage} />}
-        {pannelPage === "create-account-email-success" && <AccountEmailSent setPannelPage={setPannelPage} />}
+        {pannelPage === "create-account-email-success" && <AccountCreationEmailSent setPannelPage={setPannelPage} />}
         {pannelPage === "create-account-success" && <AccountCreationSuccess setPannelPage={setPannelPage} />}
         {pannelPage === "reset-pwd-info" && <ResetPwdUserEmail setPannelPage={setPannelPage} />}
-        {pannelPage === "reset-pwd-email-success" && <ResetPwdSuccess setPannelPage={setPannelPage} />}
+        {pannelPage === "reset-pwd-email-success" && <ResetPwdEmailSent setPannelPage={setPannelPage} />}
         {pannelPage === "reset-pwd" && <ResetPwd setPannelPage={setPannelPage} />}
         {pannelPage === "reset-pwd-success" && <ResetPwdSuccess setPannelPage={setPannelPage} />}
       </Container>
