82bf88e0869371454175bed01183e4df224aa33d
M-Kwak <prospirev@gmail.com>
2024-08-18 17:55:34 +0200

    fix indent in ResetPwdUSerEmail

--- src/pages/login/LoginactionPannel/resetPassword/resetPwdUserEmail/ResetPwdUserEmail.tsx
+++ src/pages/login/LoginactionPannel/resetPassword/resetPwdUserEmail/ResetPwdUserEmail.tsx
@@ -17,7 +17,10 @@ function ResetPwdUserEmail(props: ResetPwdUserEmailSpecs): React.JSX.Element {
       id="ResetPwdUserEmail"
     >
       <h1>Reset password</h1>
-      <Container direction="column" id="content">
+      <Container
+        direction="column"
+        id="content"
+      >
         <p>Enter your email address and get a link to reset your password</p>
         <form>
           <StyledInput
