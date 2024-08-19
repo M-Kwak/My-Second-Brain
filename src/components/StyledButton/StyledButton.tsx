450c1b593897e63b1ad11f2c131718e9af494315
M-Kwak <prospirev@gmail.com>
2024-08-19 22:19:59 +0200

    Styled button (#19)
    * fix unapropriate eol in StyledSubmitInput interface
    
    * Add StyledButton component + css / responsive

---
+++ src/components/StyledButton/StyledButton.tsx
@@ -0,0 +1,24 @@
+import "./StyledButton.css";
+
+interface StyledButtonSpecs {
+  id?: string,
+  text: string,
+  onClick: () => void,
+}
+
+function StyledButton(props: StyledButtonSpecs) {
+  const { text, id, onClick } = props;
+  return (
+    <>
+      <button
+        id={id}
+        className="styledButton"
+        onClick={onClick}
+      >
+        {text}
+      </button>
+    </>
+  );
+}
+
+export default StyledButton;
