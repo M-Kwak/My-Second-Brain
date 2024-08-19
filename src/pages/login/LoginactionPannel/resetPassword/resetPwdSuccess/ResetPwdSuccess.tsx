69eb721c0afc503953433990d12b964378c334df
M-Kwak <prospirev@gmail.com>
2024-08-19 22:05:27 +0200

    Reset pwd success (#18)
    * fix bad name props for StyledSubmitButton in AccountCreationSuccess
    
    * Add component + css / responsive  to reset password success

--- src/pages/login/LoginactionPannel/resetPassword/resetPwdSuccess/ResetPwdSuccess.tsx
+++ src/pages/login/LoginactionPannel/resetPassword/resetPwdSuccess/ResetPwdSuccess.tsx
@@ -1,6 +1,8 @@
 import { Dispatch, SetStateAction } from "react";
-import Container from "../../../../../components/container/Container";
 import { loginPageContent } from "../../../../../types/types";
+import Container from "../../../../../components/container/Container";
+import StyledSubmitInput from "../../../../../components/styledSubmitInput.tsx/StyledSubmitInput";
+import "./ResetPwdSuccess.css";
 
 interface ResetPwdSuccessSpecs {
   setPannelPage: Dispatch<SetStateAction<loginPageContent>>,
@@ -9,10 +11,24 @@ interface ResetPwdSuccessSpecs {
 function ResetPwdSuccess(props: ResetPwdSuccessSpecs): React.JSX.Element {
   const { setPannelPage } = props;
   return (
-    <Container direction="column">
-      <p>RESET PWD SUCCESS</p>
-      <p onClick={() => setPannelPage("login")}>GO BACK</p>
-    </Container>
+    <Container
+      direction="column"
+      id="ResetPwdSuccess"
+    >
+      <h1>Reset password</h1>
+      <Container
+        direction="column"
+        id="content"
+      >
+        <img src="/images/tickCircle.svg" />
+        <p>Your password has been reset with success !</p>
+        <StyledSubmitInput
+          name="ResetPwd"
+          value="Back to login"
+          onClick={() => setPannelPage("login")}
+        />
+      </Container>
+    </Container> 
   );
 }
 
