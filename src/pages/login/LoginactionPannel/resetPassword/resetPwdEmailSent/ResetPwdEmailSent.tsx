c13bc5fa16bebb3940b4455a43452b906471b610
M-Kwak <prospirev@gmail.com>
2024-08-19 20:52:18 +0200

    Add component for password reset email success + css / responsive

--- src/pages/login/LoginactionPannel/resetPassword/resetPwdEmailSent/ResetPwdEmailSent.tsx
+++ src/pages/login/LoginactionPannel/resetPassword/resetPwdEmailSent/ResetPwdEmailSent.tsx
@@ -1,6 +1,8 @@
 import { Dispatch, SetStateAction } from "react";
-import Container from "../../../../../components/container/Container";
 import { loginPageContent } from "../../../../../types/types";
+import Container from "../../../../../components/container/Container";
+import StyledSubmitInput from "../../../../../components/styledSubmitInput.tsx/StyledSubmitInput";
+import "./ResetPwdEmailSent.css";
 
 interface ResetPwdEmailSentSpecs {
   setPannelPage: Dispatch<SetStateAction<loginPageContent>>,
@@ -8,10 +10,26 @@ interface ResetPwdEmailSentSpecs {
 
 function ResetPwdEmailSent(props: ResetPwdEmailSentSpecs): React.JSX.Element {
   const { setPannelPage } = props;
+  const emailTest: string = "test@test.com";
+
   return (
-    <Container direction="column">
-      <p>RESET PWD EMAIL SENT</p>
-      <p onClick={() => setPannelPage("login")}>GO BACK</p>
+    <Container
+      direction="column"
+      id="ResetPwdEmailSent"
+    >
+      <h1>Reset password</h1>
+      <Container
+        direction="column"
+        id="content"
+      >
+        <img src="/images/emailSuccess.svg" />
+        <p>An email has been sent to : {emailTest} <br/> Follow the link inside to reset your password.</p>
+        <StyledSubmitInput
+          name="ResetPwdEmailSent"
+          value="Back to login"
+          onClick={() => setPannelPage("login")}
+        />
+      </Container>
     </Container>
   );
 }
