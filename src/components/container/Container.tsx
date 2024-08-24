9e10452492fcc5c2bf9390757eb2a6da48abc8ed
M-Kwak <prospirev@gmail.com>
2024-08-24 21:50:47 +0200

    Add onClick props in container component

--- src/components/container/Container.tsx
+++ src/components/container/Container.tsx
@@ -7,16 +7,18 @@ interface ContainerSpecs {
   direction: direction,
   id?: string,
   className?: string;
+  onClick?: () => void;
 }
 
 function Container(props: ContainerSpecs): React.JSX.Element {
-  const { children, direction, id, className } = props;
+  const { children, direction, id, className, onClick } = props;
 
   return (
     <div
       id={id}
       className={`container ${className}`}
       style={{ flexFlow: `${direction} wrap` }}
+      onClick={onClick}
     >
       {children}
     </div>
