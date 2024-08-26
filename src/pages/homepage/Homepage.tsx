f75cdbccc72cbd3aaeba722d7e3ce520ecf1c182
M-Kwak <prospirev@gmail.com>
2024-08-26 22:00:37 +0200

    Homepage style (#24)
    * Add icons for apps.
    
    * Change icons in homepage and add style + repsonsive

--- src/pages/homepage/Homepage.tsx
+++ src/pages/homepage/Homepage.tsx
@@ -11,45 +11,49 @@ function Homepage() {
       direction="column"
       id="HomePage"
     >
-      <header></header>
+      <header>
+        <img src="/images/hamburgerIcon.svg" />
+        <img src="/images/accountIcon.svg" />
+      </header>
       <main>
         {selectedApp === null && (
           <>
-            <Container
-              className="appIconContainer"
-              direction="row"
-              onClick={() => setSelectedApp('archive')}
-            >
-              <img src="/images/brainIcon.svg" />
-            </Container>
-            <Container
-              className="appIconContainer"
-              direction="row"
-              onClick={() => setSelectedApp('calendar')
-
-              }>
-              <img src="/images/calendarIcon.svg" />
-            </Container>
-            <Container
-              className="appIconContainer"
-              direction="row"
-              onClick={() => setSelectedApp('to-do')}
-            >
-              <img src="/public/images/todolistIcon.svg" />
-            </Container>
-            <Container
-              className="appIconContainer"
-              direction="row"
-              onClick={() => setSelectedApp('vault')}
-            >
-              <img src="/images/vaultIcon.svg" />
-            </Container>
-            <Container
-              className="appIconContainer"
-              direction="row"
-              onClick={() => setSelectedApp('progress')}
-            >
-              <img src="/images/barbellIcon.svg" />
+            <Container id="appMainContainer" direction="row">
+              <Container
+                className="appIconContainer"
+                direction="row"
+                onClick={() => setSelectedApp('archive')}
+              >
+                <img src="/images/brainIcon.svg" />
+              </Container>
+              <Container
+                className="appIconContainer"
+                direction="row"
+                onClick={() => setSelectedApp('calendar')
+                }>
+                <img src="/images/calendarIcon.svg" style={{ width: '50px', height: '50px' }} />
+              </Container>
+              <Container
+                className="appIconContainer"
+                direction="row"
+                onClick={() => setSelectedApp('to-do')}
+              >
+                <img src="/images/todolistIcon.svg" />
+              </Container>
+              <Container
+                className="appIconContainer"
+                direction="row"
+                onClick={() => setSelectedApp('vault')}
+              >
+                <img src="/images/vaultIcon.svg" />
+              </Container>
+              <Container
+                className="appIconContainer"
+                direction="row"
+                onClick={() => setSelectedApp('progress')}
+              >
+                <img src="/images/barbellIcon.svg" />
+              </Container>
             </Container>
           </>
         )}
