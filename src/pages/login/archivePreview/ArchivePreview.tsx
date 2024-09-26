a449c52c6a8cdf9f13e43e56cc96f53c2db4a3e6
M-Kwak <prospirev@gmail.com>
2024-09-26 23:34:47 +0200

    Add login preview real vault app preview and on progress for unfinished ones. (#46)

--- src/pages/login/archivePreview/ArchivePreview.tsx
+++ src/pages/login/archivePreview/ArchivePreview.tsx
@@ -1,9 +1,8 @@
-import ArchiveTopicPreview from "../../../components/archiveTopicPreview/ArchiveTopicPreview";
 import Container from "../../../components/container/Container";
 
 function ArchivePreview() {
-    return (
-      <Container
+  return (
+    <Container
       direction="column"
       className="appPreviewContainer"
     >
@@ -17,41 +16,13 @@ function ArchivePreview() {
         />
         <h3>Archive your knowledge</h3>
       </Container>
-      <Container
-        direction="column"
-        className="innerAppPreviewContainer"
-      >
-        <p style={{ marginBottom: 0 }}>Create topics</p>
-        <Container direction="row">
-          <ArchiveTopicPreview
-            className="archiveTopicExemple"
-            name="Painting"
-            chapterNumber={14}
-            icon="/images/paintingArt.svg"
-          />
-          <ArchiveTopicPreview
-            className="archiveTopicExemple"
-            name="Cooking"
-            chapterNumber={3}
-            icon="/images/cookHat.svg"
-          />
-          <ArchiveTopicPreview
-            className="archiveTopicExemple"
-            name="Astronomy"
-            chapterNumber={9}
-            icon="/images/telescope.svg"
-          />
-        </Container>
-      </Container>
-      <Container
-        direction="column"
-        className="innerAppPreviewContainer"
-      >
-        <p>And fill it with chapter, images and texts.</p>
-        <div className="appPreview">ARCHIVE APP EXEMPLE</div>
-      </Container>
+      <img
+        className="appPreview"
+        src="/images/archivePreview.svg"
+        alt="vault app preview"
+      />
     </Container>
-    )
+  )
 }
 
 export default ArchivePreview;
